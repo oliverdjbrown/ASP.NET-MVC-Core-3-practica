a79a193bc600ceb2d2f184c6ba3438782ffa1689
Oliver Brown <oliverbrodev@gmail.com>
2020-08-23 20:30:06 -0400

    Enrutamiento de atributos #1

--- Usuarios/Controllers/UsuariosController.cs
+++ Usuarios/Controllers/UsuariosController.cs
@@ -6,14 +6,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Usuarios.Controllers
 {
+    [Route("/Nombre")]
     public class UsuariosController : Controller
     {
         //[HttpGet]
+        [Route("/Nombre/Oliver")]
         public IActionResult Index(String data, int age)
         {
             //ViewData["id"] = data + " " + age;
             String datos = data + " " + age;
             return View("Index", datos);
         }
+
+        public IActionResult Index()
+        {            
+            return View();
+        }
     }
 }
+ 
