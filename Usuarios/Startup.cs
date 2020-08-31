225b7081e7c97b14ecce471416034d2d7df2cd15
Oliver Brown <oliverbrodev@gmail.com>
2020-08-31 06:08:12 -0400

    Creando la área usuario y su controlador usuarios

--- Usuarios/Startup.cs
+++ Usuarios/Startup.cs
@@ -95,7 +95,8 @@ namespace Usuarios
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-                    //pattern: "{controller=Usuarios}/{action=Index}/{id?}");
+                //pattern: "{controller=Usuarios}/{action=Index}/{id?}");
+                endpoints.MapAreaControllerRoute("Usuarios", "Usuario", "{controller=Usuario}/{action=Usuario}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
